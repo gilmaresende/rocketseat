27f7e1f2f673c967e4bd32c83cae88a2ed1205c7
Gilmar F <gilmar74796@gmail.com>
2020-08-18 12:27:07 -0300

    Start curso-es6

---
+++ curso-es6/src/main.js
@@ -0,0 +1,15 @@
+const minhaPromise = () =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("ok");
+    }, 2000);
+  });
+
+async function executaPromise(){
+    console.log(await minhaPromise());
+    console.log(await minhaPromise());
+    console.log(await minhaPromise());
+
+}
+
+executaPromise();
