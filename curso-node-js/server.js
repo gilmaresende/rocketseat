b50be0cb11ef834848d32c679806f6f4048cbc93
Gilmar F <gilmar74796@gmail.com>
2020-08-23 19:04:52 -0300

    Conectando Database (MongoDB)
    aula 7

--- curso-node-js/server.js
+++ curso-node-js/server.js
@@ -1,7 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
+//Iniciando App
 const app = express();
 
+//Iniciando DB
+mongoose.connect("mongodb://localhost:27017/nodeapi", {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+
+//Primeira rota
 app.get("/", (req, res) => {
   res.send("Hello world.");
 });
