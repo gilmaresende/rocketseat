50127379bc9ee40aaf0233e6bfa014fba56e6ddb
Gilmar F <gilmar74796@gmail.com>
2020-08-23 16:18:50 -0300

    Aplicação com ES6+ <Buscando da API>
    modulo 4 aula 4

--- curso-es6/src/main.js
+++ curso-es6/src/main.js
@@ -1,16 +1,71 @@
-import axios from "axios";
+import api from "./api";
+class App {
+  constructor() {
+    this.repository = [];
+    this.formEl = document.getElementById("repo-form");
+    this.inputEl = document.querySelector("input[name=repository]");
+    this.listEl = document.getElementById("repo-list");
 
-class Api {
-  static async getUsuarioInfo(userName) {
+    this.registerHandlers();
+  }
+
+  registerHandlers() {
+    this.formEl.onsubmit = (event) => this.addRepository(event);
+  }
+
+  async addRepository(event) {
+    event.preventDefault();
+
+    const repoInput = this.inputEl.value;
+
+    if (repoInput.length === 0) return;
+
     try {
-      const reposnse = await axios.get(
-        `https://api.github.com/users/${userName}`
-      );
-      console.log(reposnse);
+      const response = await api.get(`repos/${repoInput}`);
+
+      const {
+        name,
+        description,
+        html_url,
+        owner: { avatar_url },
+      } = response.data;
+
+      this.repository.push({ name, description, avatar_url, html_url });
+      this.inputEl.value = "";
+
+      this.render();
     } catch (err) {
-      console.warn("Erro na API");
+      console.log("Não encontrado");
     }
+  }
+
+  render() {
+    this.listEl.innerHTML = "";
+
+    this.repository.forEach((repo) => {
+      let imgEl = document.createElement("img");
+      imgEl.setAttribute("src", repo.avatar_url);
+
+      let titleEl = document.createElement("strong");
+      titleEl.appendChild(document.createTextNode(repo.name));
+
+      let descriptionEl = document.createElement("p");
+      descriptionEl.appendChild(document.createTextNode(repo.description));
+
+      let linkEl = document.createElement("a");
+      linkEl.setAttribute("target", "_blank");
+      linkEl.setAttribute("href", repo.html_url);
+      linkEl.appendChild(document.createTextNode("Acessar"));
+
+      let listItemEl = document.createElement("li");
+      listItemEl.appendChild(imgEl);
+      listItemEl.appendChild(titleEl);
+      listItemEl.appendChild(descriptionEl);
+      listItemEl.appendChild(linkEl);
+
+      this.listEl.appendChild(listItemEl);
+    });
   }
 }
 
-Api.getUsuarioInfo("gilmaresende");
+new App();
